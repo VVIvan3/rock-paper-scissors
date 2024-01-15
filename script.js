23c9b0d55c006b796cd7abfb75c39277450942fb
VVIvan3 <vanya.vorobiev1@mail.ru>
2024-01-15 11:26:33 +0300

    added semicolons
    added semicolons to the places, where they were omitted

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function getComputerChoice() {
     let randInt = Math.floor(Math.random() * 3);
     if (randInt === 0) {
-        return 'rock'
+        return 'rock';
     } else if (randInt === 1) {
-        return 'paper'
+        return 'paper';
     } else {
-        return 'scissors'
+        return 'scissors';
     };
 }
 
@@ -17,37 +17,37 @@ function playRound() {
     computerSelection = getComputerChoice();
     playerSelection = getPlayerChoice();
     if (playerSelection === computerSelection) {
-        return 0
+        return 0;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return 2
+        return 2;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return 2
+        return 2;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return 2
+        return 2;
     } else {
-        return 1
+        return 1;
     }
 }
 
 function game() {
-    let playerScore = 0
-    let computerScore = 0
-    let onGoing = true
+    let playerScore = 0;
+    let computerScore = 0;
+    let onGoing = true;
     while (onGoing) {
         let round = playRound()
         if (round === 1) {
-            ++computerScore
+            ++computerScore;
         } else if (round === 2) {
-            ++playerScore
-        }
-    console.log(`Player score is: ${playerScore} Computer score is: ${computerScore}`)
+            ++playerScore;
+        };
+    console.log(`Player score is: ${playerScore} Computer score is: ${computerScore}`);
     if (playerScore === 5 ) {
         console.log('Player won!');
-        onGoing = false
+        onGoing = false;
     } else if (computerScore === 5) {
         console.log('Computer won!');
-        onGoing = false
+        onGoing = false;
     }
     }
 }
-game()
+game();
